0bfafaff4b2585a462db0c97c821c00c3a3ab8d2
beynar <arnaud@derbey.dev>
2025-08-31 12:36:42 +0200

    add dist to gitignore

--- dist/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const colorSchemes: readonly ["light", "dark"];
-export type ColorScheme = (typeof colorSchemes)[number];
-export declare const MEDIA = "(prefers-color-scheme: dark)";
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import {} from 'svelte';
-// Export the main component for external consumption (library entry point)
-export { default as SvelteTheme } from './SvelteTheme.svelte';
-export { useTheme } from './theme.state.svelte';
